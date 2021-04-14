85ea77d3f265a5e6e13146ad0aef48e60c6ebcbe
David Kim <hola.hoon@gmail.com>
2021-04-14 14:06:29 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Webpack 5 project setup tutorial practice
+
+learned from [Youtube](https://www.youtube.com/watch?v=TOb1c39m64A&t=180s)
---
+++ src/index.js
@@ -0,0 +1,7 @@
+const elvenShieldRecipe = {
+  leatherStripes: 2,
+  ironIngot: 1,
+  refinedMoonstrong: 4,
+};
+
+console.log(elvenShieldRecipe);
